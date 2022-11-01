c2881759675677050f23eee1cd591a767e897163
Tokiwokoe <mam4ur.anastasia@gmail.com>
2022-11-01 17:13:46 +0300

    full_client_version

--- connection.py
+++ connection.py
@@ -2,8 +2,10 @@ import os
 
 import psycopg2
 
-connection = psycopg2.connect(host=os.environ.get('HOST'),
-                        database=os.environ.get('DATABASE'),
-                        user=os.environ.get('USER'),
-                        password=os.environ.get('PASSWORD'))
+connection = psycopg2.connect(
+    host=os.environ.get('HOST'),
+    database=os.environ.get('DATABASE'),
+    user=os.environ.get('USER'),
+    password=os.environ.get('PASSWORD')
+)
 
